87e2312e2f095ecec500e28143825d42728ac820
Vanessa Yeoh <vyeoh@Vanessas-MacBook-Pro.local>
2019-01-10 23:34:43 +0800

    added first, init and data callbacks:

--- js/app.js
+++ js/app.js
@@ -4,9 +4,25 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
   $scope.items = [ { "_id": "5c375cdeb9065d2757749948", "age": 23, "eyeColor": "brown", "name": "Robbins Obrien", "gender": "male", "company": "ICOLOGY", "email": "robbinsobrien@icology.com", "tags": [ "ut", "reprehenderit", "nostrud", "proident", "irure", "eiusmod", "do" ] }, { "_id": "5c375cde570ac20aaa5795c1", "age": 21, "eyeColor": "green", "name": "Ola Montgomery", "gender": "female", "company": "LYRICHORD", "email": "olamontgomery@lyrichord.com", "tags": [ "in", "et", "magna", "sint", "do", "minim", "mollit" ] }, { "_id": "5c375cde5ed613ef885eb97b", "age": 29, "eyeColor": "green", "name": "Puckett Fields", "gender": "male", "company": "ENTROFLEX", "email": "puckettfields@entroflex.com", "tags": [ "amet", "culpa", "aute", "sit", "eu", "pariatur", "laboris" ] } ];
 
   branch.init('key_live_hkDytPACtipny3N9XmnbZlapBDdj4WIL', options, function(err, data) {
-    console.log(err, data);
+    if(!err){
+      console.log("Init:", data)
+    }
   });
-  
+
+  branch.data(function(err, data) {
+    if(!err){
+      console.log("Collected Data:", data)
+    }
+  });
+  First data
+
+
+  branch.first(function(err, data) {
+    if(!err){
+      console.log("First Event:", data)
+    }
+  });
+
   $scope.createLink = function(item){
     var linkData = {
     campaign: 'content 123',
