4c74e35a27a9695a0a1760a1a97d329df11937de
Vanessa Yeoh <vyeoh@Vanessas-MacBook-Pro.local>
2019-01-11 00:01:57 +0800

    test send sms

--- js/app.js
+++ js/app.js
@@ -24,31 +24,58 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
 
   $scope.createLink = function(item){
     var linkData = {
-    campaign: 'content 123',
-    channel: 'facebook',
-    feature: 'dashboard',
-    stage: 'new user',
-    tags: [ 'tag1', 'tag2', 'tag3' ],
-    alias: '',
-    data: {
-      'custom_bool': true,
-      'custom_int': Date.now(),
-      '$og_title': item.name,
-      '$og_description': 'Description',
-      '$og_image_url':'http://lorempixel.com/400/400',
-      'gender':item.gender,
-      'tags': item.tags
-    }
-  };
-  console.log(linkData);
-  branch.link(linkData, function(err, link) {
-    // bind elements
-    if(!err){
-      window.open(link);
-    }
+      campaign: 'content 123',
+      channel: 'facebook',
+      feature: 'dashboard',
+      stage: 'new user',
+      tags: [ 'tag1', 'tag2', 'tag3' ],
+      alias: '',
+      data: {
+        'custom_bool': true,
+        'custom_int': Date.now(),
+        '$og_title': item.name,
+        '$og_description': 'Description',
+        '$og_image_url':'http://lorempixel.com/400/400',
+        'gender':item.gender,
+        'tags': item.tags
+      }
+    };
+    console.log(linkData);
+    branch.link(linkData, function(err, link) {
+      // bind elements
+      if(!err){
+        window.open(link);
+      }
 
 
 
-  });
+    });
+  }
+  $scope.sendSMS = function(){
+    var phoneNumber = '+6592976896' // +919812345678, +442071234567
+    var linkData = {
+      campaign: 'content 123',
+      channel: 'facebook',
+      feature: 'dashboard',
+      stage: 'new user',
+      tags: [ 'tag1', 'tag2', 'tag3' ],
+      alias: '',
+      data: {
+        'custom_bool': true,
+        'custom_int': Date.now(),
+        'custom_string': 'hello',
+        '$og_title': 'Title',
+        '$og_description': 'Description',
+        '$og_image_url':'http://lorempixel.com/400/400'
+      }
+    };
+
+    var linkOptions = {
+      make_new_link: false // don't create a new deep link if one already exists (e.g. _branch_match_id is in the address bar)
+    };
+
+    branch.sendSMS(phoneNumber, linkData, linkOptions, function(err, data) {
+      console.log(err);
+    });
   }
 }]);
